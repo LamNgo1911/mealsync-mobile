710a285b9f1b435ae7a1d01adc251952532b7041
Diddayngo <lamngo606@gmail.com>
2025-06-23 23:29:39 +0300

    Update: update tailwinds

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex justify-center items-center flex-1">
-      <Text>Home - MealSync</Text>
+      <Text className="text-lg font-bold">Home - MealSync</Text>
     </View>
   );
 }
